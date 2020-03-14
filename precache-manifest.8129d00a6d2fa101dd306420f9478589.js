b342b79bf59b742bc915e8b84628ece0d6bce841
Julio Cezar Taveira Araujo <jctaraujo@hotmail.com>
2020-03-14 15:11:46 -0300

    Updates

---
+++ precache-manifest.8129d00a6d2fa101dd306420f9478589.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adbd31bf9c1c5f9fc779b4b26a30e44f",
+    "url": "/Talentos-Digitais-Localiza/index.html"
+  },
+  {
+    "revision": "8849b2ec2e1644f08a51",
+    "url": "/Talentos-Digitais-Localiza/static/css/main.5c5bb842.chunk.css"
+  },
+  {
+    "revision": "ea1e9f3e31254ea6c599",
+    "url": "/Talentos-Digitais-Localiza/static/js/2.a54e77ed.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Talentos-Digitais-Localiza/static/js/2.a54e77ed.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8849b2ec2e1644f08a51",
+    "url": "/Talentos-Digitais-Localiza/static/js/main.44ee3bd0.chunk.js"
+  },
+  {
+    "revision": "744ea1120ebdb47cc7d8",
+    "url": "/Talentos-Digitais-Localiza/static/js/runtime-main.2cda447e.js"
+  },
+  {
+    "revision": "0f7e3fa77a8d60b935d47b1b296be155",
+    "url": "/Talentos-Digitais-Localiza/static/media/background_star_wars.0f7e3fa7.jpg"
+  }
+]);
